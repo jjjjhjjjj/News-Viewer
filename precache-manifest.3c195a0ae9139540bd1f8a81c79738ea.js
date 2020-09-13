8b05296ac9014ecf31117310d858713377a41a0b
hj <jjjjhjjjj123@gmail.com>
2020-09-14 02:57:36 +0900

    Updates

---
+++ precache-manifest.3c195a0ae9139540bd1f8a81c79738ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff245a628ef294377664fabf7e1e3a6d",
+    "url": "/News-Viewer/index.html"
+  },
+  {
+    "revision": "9a54293ab7d807f75837",
+    "url": "/News-Viewer/static/css/main.2daf2037.chunk.css"
+  },
+  {
+    "revision": "ed48b7be6d089a49b6f4",
+    "url": "/News-Viewer/static/js/2.31475ad8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/News-Viewer/static/js/2.31475ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a54293ab7d807f75837",
+    "url": "/News-Viewer/static/js/main.3a3b8268.chunk.js"
+  },
+  {
+    "revision": "6ccc6d6a1f9fcae77bb0",
+    "url": "/News-Viewer/static/js/runtime-main.64dbc316.js"
+  }
+]);
